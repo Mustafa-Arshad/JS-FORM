687ec24515c4ffc6dddd87ef422f28c7b5e26e09
Mustafa-Arshad <hafiz28mustafa@gmail.com>
2022-11-28 17:19:22 +0500

    work done

---
+++ app.js
@@ -0,0 +1,100 @@
+var text = document.getElementById('inp');
+var errorPera = document.getElementById('error');
+
+var arr = [];
+
+function addme() {
+    var b = text.value;
+    if (b == '') {
+        errorPera.innerHTML = "enter Name"
+    }
+    else {
+        arr.push(b);
+        console.log(arr);
+    }
+}
+
+var text = document.getElementById('nip');
+var errorPera = document.getElementById('error1');
+
+var arr = [];
+
+function addme() {
+    var b = text.value;
+    if (b == '') {
+        errorPera.innerHTML = "enter user Name"
+    }
+    else {
+        arr.push(b);
+        console.log(arr);
+    }
+}
+
+
+
+
+
+
+var text = document.getElementById('pin');
+var errorPera = document.getElementById('error2');
+
+var arr = [];
+
+function addme() {
+    var b = text.value;
+    if (b == '') {
+        errorPera.innerHTML = "enter your email"
+    }
+    else {
+        arr.push(b);
+        console.log(arr);
+    }
+
+
+}
+
+var text = document.getElementById('abc');
+var errorPera = document.getElementById('error3');
+
+var arr = [];
+
+function addme() {
+    var b = text.value;
+    if (b == '') {
+        errorPera.innerHTML = "enter your password"
+    }
+    else {
+        arr.push(b);
+        console.log(arr);
+    }
+}
+var text = document.getElementById('abd');
+var errorPera = document.getElementById('error4');
+
+var arr = [];
+
+function addme() {
+    var b = text.value;
+    if (b == '') {
+        errorPera.innerHTML = "enter your number"
+    }
+    else {
+        arr.push(b);
+        console.log(arr);
+    }
+}
+var text = document.getElementById('abe');
+var errorPera = document.getElementById('error5');
+
+var arr = [];
+
+function addme() {
+    var b = text.value;
+    if (b == '') {
+        errorPera.innerHTML = "enter your Cnic"
+    }
+    else {
+        arr.push(b);
+        console.log(arr);
+    }
+}
